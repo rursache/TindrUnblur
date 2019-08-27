913f9409616d0dcbbd45c0e64b2856c3d463c8ce
Radu Ursache <radu@ursache.ro>
2019-08-27 21:18:22 +0300

    - added loading state

--- content.js
+++ content.js
@@ -2,8 +2,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
   	chrome.tabs.getSelected(null,function(tab) {
   		if (tab.url.includes("app/likes-you")) {
-  			// setTimeout(showLoading(), 500);
-  			setTimeout(unlockMatches(), 3000);
+  			setTimeout(function(){ showLoading(); }, 500);
+  			setTimeout(function(){ unlockMatches(); }, 3000);
   		}
 	});
   }
@@ -16,7 +16,7 @@ function showLoading() {
 
 function unlockMatches() {
 	chrome.tabs.executeScript(null, 
-		{code:"document.body.innerHTML = document.body.innerHTML.replace('Upgrade to Gold to see people who already liked you.', 'Enjoy the magic of TindrUnblur 🔥')"});
+		{code:"document.body.innerHTML = document.body.innerHTML.replace('Loading TindrUnblur, please wait...', 'Enjoy the magic of TindrUnblur 🔥')"});
 
 	chrome.tabs.executeScript(null, 
 		{code:"document.body.innerHTML = document.body.innerHTML.replace('><span class=\"button__text Pos(r) Z(1)\">See Who Likes You</span></button>', 'hidden=\"true\"><span class=\"button__text Pos(r) Z(1)\">See Who Likes You</span></button>')"});
